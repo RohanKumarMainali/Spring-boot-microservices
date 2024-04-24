d7eeee3db19b7dc03b9b5a1a9312fbee8fce14e6
RohanKumarMainali <rohan.manali@aveshasystems.com>
2024-04-24 10:36:41 +0545

    fix(): call userRatings service

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingDataResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingDataResource.java
@@ -1,10 +1,13 @@
 package io.javabrains.ratingsdataservice.resources;
 
+import java.util.Arrays;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.javabrains.ratingsdataservice.model.Rating;
+import io.javabrains.ratingsdataservice.model.UserRating;
 
 @RestController
 @RequestMapping("/ratings")
@@ -13,6 +16,18 @@ public class RatingDataResource {
   @RequestMapping("/{movieId}")
   public Rating getRatingData(@PathVariable("movieId") String movieId) {
     return new Rating(movieId, 4);
+  }
+
+  @RequestMapping("/users/{userId}")
+  public UserRating getUserRating(@PathVariable("userId") String userId) {
+
+    UserRating userRating = new UserRating(
+        userId,
+        Arrays.asList(
+            new Rating("100", 3),
+            new Rating("200", 4)));
+
+    return userRating;
   }
 
 }
